b39ded37f943cc8b55b7a147bee4c6374c246386
Gerry Yang <gerry04y@gmail.com>
2022-12-03 16:48:14 +0000

    Piano Sidebar Negative Margins

---
+++ src/components/PianoSidebar.js
@@ -0,0 +1,26 @@
+function PianoSidebar() {
+	return (
+		<div className="w-1/3">
+			<div className="grid grid-cols-4 sticky top-0">
+				<button className="white-piano-key">Content 1</button>
+				<button className="black-piano-key">Content 2</button>
+				<button className="white-piano-key">Content 3</button>
+				<button className="black-piano-key">Content 4</button>
+				<button className="white-piano-key">Content 5</button>
+				<button className="white-piano-key">Content 1</button>
+				<button className="black-piano-key">Content 2</button>
+				<button className="white-piano-key">Content 3</button>
+				<button className="black-piano-key">Content 4</button>
+				<button className="white-piano-key">Content 5</button>
+				<button className="black-piano-key">Content 2</button>
+				<button className="white-piano-key">Content 3</button>
+				<button className="white-piano-key">Content 1</button>
+				<button className="black-piano-key">Content 2</button>
+				<button className="white-piano-key">Content 3</button>
+				<button className="black-piano-key">Content 4</button>
+				<button className="white-piano-key">Content 5</button>
+			</div>
+		</div>
+	)
+}
+export default PianoSidebar;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,30 +1,24 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+import PianoSidebar from './PianoSidebar';
 function Timeline() {
-	useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					const buttonId = "piano-key-" + entry.target.id;
-					const button = document.getElementById(buttonId);
-					button.className = "bg-yellow-100";
-				}
-			})
-		}, []);
-		const contents = document.querySelectorAll("h1");
-		contents.forEach((content) => {
-			observer.observe(content);
-		})
-	}, [])
+	// useEffect(() => {
+	// 	const observer = new IntersectionObserver((entries) => {
+	// 		entries.forEach((entry) => {
+	// 			if (entry.isIntersecting) {
+	// 				const buttonId = "piano-key-" + entry.target.id;
+	// 				const button = document.getElementById(buttonId);
+	// 				button.className = "bg-yellow-100";
+	// 			}
+	// 		})
+	// 	}, []);
+	// 	const contents = document.querySelectorAll("h1");
+	// 	contents.forEach((content) => {
+	// 		observer.observe(content);
+	// 	})
+	// }, [])
 	return (
-		<div className="flex flex-row border-black border-2 m-20">
-			<div className="w-1/3 border-yellow-500 border-2">
-				<div className="flex flex-col sticky top-28 border-2">
-					<button id="piano-key-content-1" className="piano-key">Content 1</button>
-					<button id="piano-key-content-2" className="piano-key">Content 2</button>
-					<button id="piano-key-content-3" className="piano-key">Content 3</button>
-					<button id="piano-key-content-4" className="piano-key">Content 4</button>
-				</div>
-			</div>
+		<div className="flex flex-row m-20">
+			<PianoSidebar/>
 			<div className="w-2/3 border-black border-2">
 				<div>
 					<h1 id="content-1" className="content-1 font-bold">Content 1</h1>
