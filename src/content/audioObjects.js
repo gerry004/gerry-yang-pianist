2bc4c3ae5f4ed7994fcf3591f211812db74a7d92
Gerry Yang <gerry04y@gmail.com>
2022-12-12 14:21:12 +0000

    Trimmed Audios

--- src/content/audioObjects.js
+++ src/content/audioObjects.js
@@ -1,38 +1,37 @@
 import brahmsSonata from '../assets/audio/brahms-sonata.mp3';
 import chopinRevolutionary from '../assets/audio/chopin-revolutionary.mp3';
-import lisztRigoletto from '../assets/audio/liszt-rigoletto.MOV.mp3';
+import lisztRigoletto from '../assets/audio/liszt-rigoletto.mp3';
 import lisztRhapsody from '../assets/audio/list-hungarian-rhapsody-2.mp3';
 import tchaikovskyConcerto from '../assets/audio/tchaik-concerto-1.mp3';
-import griegHomesickness from '../assets/audio/grieg-homesickness.MOV.mp3';
-import mozartConcerto from '../assets/audio/mozart-concerto-8.MOV.mp3';
+import griegHomesickness from '../assets/audio/grieg-homesickness.mp3';
+import mozartConcerto from '../assets/audio/mozart-concerto.mp3';
 import rachMomentMusicaux from '../assets/audio/rach-moment-musicaux.mp3';
 import schumannAufschwung from '../assets/audio/schumann-aufschwung.mp3';
 import schumannDesAbends from '../assets/audio/schumann-des-abends.mp3';
-import lisztPaganiniCaprice from '../assets/audio/liszt-paganini-caprice-5.MOV.mp3';
-import chopinTristesse from '../assets/audio/chopin-etude-10-3.MOV.mp3';
+import lisztPaganiniCaprice from '../assets/audio/liszt-paganini-caprice-5.mp3';
+import chopinTristesse from '../assets/audio/chopin-etude-10-3.mp3';
 
 
 const AUDIO_OBJECTS = [
-	{
-		audio: brahmsSonata,
-		title: "Sonata No.1 First Movement",
-		composer: "Brahms"
-	},
 	{
 		audio: lisztRigoletto,
 		title: "Rigoletto Paraphrase",
 		composer: "Liszt"
 	},
 	{
-		audio: chopinRevolutionary,
-		title: "Etude Op.10 No.12 'Revolutionary'",
-		composer: "Chopin"
+		audio: lisztRhapsody,
+		title: "Hungarian Rhapsody No.2",
+		composer: "Liszt"
+	},	
+	{
+		audio: brahmsSonata,
+		title: "Sonata No.1 First Movement",
+		composer: "Brahms"
 	},
 	{
-		audio: rachMomentMusicaux,
-		title: "Moment Musicaux Op.16 No.4",
-		//edit break in beginning, applause
-		composer: "Rachmaninoff"
+		audio: griegHomesickness,
+		title: "Homesickness",
+		composer: "Grieg"
 	},
 	{
 		audio: lisztPaganiniCaprice,
@@ -40,13 +39,9 @@ const AUDIO_OBJECTS = [
 		composer: "Liszt"
 	},
 	{
-		audio: griegHomesickness,
-		title: "Homesickness",
-		composer: "Grieg"
-	},	{
-		audio: lisztRhapsody,
-		title: "Hungarian Rhapsody No.2",
-		composer: "Liszt"
+		audio: mozartConcerto,
+		title: "Piano Concerto No.8 in C major 1st Movement",
+		composer: "Mozart"
 	},
 	{
 		audio: tchaikovskyConcerto,
@@ -54,25 +49,28 @@ const AUDIO_OBJECTS = [
 		composer: "Tchaikovsky"
 	},
 	{
-		audio: mozartConcerto,
-		title: "Piano Concerto No.8 in C major 1st Movement",
-		//edit
-		composer: "Mozart"
-	},	{
-		audio: schumannAufschwung,
-		title: "Fantasiestucke Op.12 'Aufschwung'",
-		// applause at the end
+		audio: schumannDesAbends,
+		title: "Fantasiestucke Op.12 'Des Abends'",
 		composer: "Schumann"
 	},
 	{
-		audio: schumannDesAbends,
-		title: "Fantasiestucke Op.12 'Des Abends'",
-		// edit
+		audio: schumannAufschwung,
+		title: "Fantasiestucke Op.12 'Aufschwung'",
 		composer: "Schumann"
+	},
+	{
+		audio: rachMomentMusicaux,
+		title: "Moment Musicaux Op.16 No.4",
+		composer: "Rachmaninoff"
 	},
 	{
 		audio: chopinTristesse,
 		title: "Etude Op.10 No.3 'Tristesse'",
+		composer: "Chopin"
+	},
+	{
+		audio: chopinRevolutionary,
+		title: "Etude Op.10 No.12 'Revolutionary'",
 		composer: "Chopin"
 	},
 ];
